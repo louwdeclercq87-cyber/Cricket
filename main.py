c9d4067efdb8054dc62b5c51ed1dfdf686afbd43
louwdeclercq87-cyber <louwdeclercq87@gmail.com>
2026-02-08 14:01:41 +0200

    Create main.py with CricketSimulator class methods

---
+++ main.py
@@ -0,0 +1,16 @@
+class CricketSimulator:
+    def initialize_game(self):
+        # Initialize game settings
+        pass
+
+    def start_match(self):
+        # Logic to start the match
+        pass
+
+    def display_menu(self):
+        # Display the game menu
+        pass
+
+    def run_game_loop(self):
+        # Main game loop
+        pass
